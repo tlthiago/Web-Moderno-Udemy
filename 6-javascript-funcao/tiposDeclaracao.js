b3666f37d2b09a7dc463cb52513384a5fa3ef5a5
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2023-05-23 18:34:08 -0300

    Commit dos estudos

---
+++ 6-javascript-funcao/tiposDeclaracao.js
@@ -0,0 +1,14 @@
+// Function declaration
+function soma () {
+    return x + y;
+}
+
+// Function expression
+const sub = function (x, y) {
+    return x - y;
+}
+
+// Named function expression
+const mult = function (x, y) {
+    return x * y;
+}
